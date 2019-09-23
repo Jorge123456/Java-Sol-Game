60cc9b623307b539cf15808f4d3eb501fcf27086
Jorge123456 <jorge.carrondo@gmail.com>
2019-09-23 19:01:42 +0100

    Update Contract.java

--- Contract.java
+++ Contract.java
@@ -1,6 +1,18 @@
-public void Contract {
+public class Contract {
 
     int cont_duration;
     String name;
+    
+}
+
+private class Contract_Admin extends Contract {
+
+    String identifier_tokenA;
+
+}
+
+private class Contract_Player extends Contract {
+
+    String identifier_tokenP;
     
 }
