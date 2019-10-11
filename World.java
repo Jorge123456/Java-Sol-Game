b3080bfcefccf201bd327b690d1a0b3ccfca39bd
Jorge123456 <11522995+Jorge123456@users.noreply.github.com>
2019-10-11 14:12:33 +0100

    Update World.java

--- World.java
+++ World.java
@@ -5,11 +5,35 @@ private class World {
 
 }
 
-
 protected class floor {
 
     int depth;
     int width;
     int total =  depth * width; 
 
+}
+
+protected class margin {
+    
+    int x_coord;
+    int y_coord;
+    int z_coord;
+    
+}
+
+public class global_structure {
+    
+    String name;
+    int serial_number;
+    ArrayList<String> properties = new ArrayList<String>();
+    /*To be added
+    String properties(String color, int durability, bool)
+    */
+}
+
+public class new_structure extends global_structure {
+    
+    String type;
+    //to implement independent properties per structure, making it unique
+    
 }
